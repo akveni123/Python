51dbd9aaf6561eed768c23f33b6568f550575266
akveni123 <akveni05@gmail.com>
2022-01-31 22:47:39 +0530

    pyzip

---
+++ zipdemo.py
@@ -0,0 +1,9 @@
+name = ("ashok", "King", "Hero","ashok")
+company = ("altair","amazon", "amepay", "altair")
+
+zipped = list(zip(name,company))
+
+# print(zipped)
+
+for (a,b) in zipped:
+    print(a,b)
